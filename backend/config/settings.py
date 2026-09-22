44e58c4a8de3058b87a1659136ed7dedf8fd0e48
Adlet <zeitengazin007@gmail.com>
2026-09-22 15:28:55 +0500

    Fix mobile Swagger UI templates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'common_api',
     'mobile_api',
     'partner_api',
