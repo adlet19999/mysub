aa68a3fe76eee5ceada726e2faa001c97ebcc2aa
Adlet <zeitengazin007@gmail.com>
2026-09-25 00:32:38 +0500

    Resolve mobile migration conflict

---
+++ backend/mobile_api/migrations/0003_merge_0002_city_directory_0002_customersubscription.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("mobile_api", "0002_city_directory"),
+        ("mobile_api", "0002_customersubscription"),
+    ]
+
+    operations = []
